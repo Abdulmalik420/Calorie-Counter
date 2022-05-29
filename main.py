3bfcc07f3457ce7dbeef2413b91fe818020a321b
Abdulmalik420 <abdulmalik.abdulazizov@gmail.com>
2022-05-29 19:45:38 -0400

    Basic users

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
 import os
-import time
+import datetime
 
-def newUserVerify(users):
+x = datetime.datetime.now()
+todayDate = x.strftime("%B %d")
+nowDate = x.strftime("%d")
+nowMonth = x.strftime("%B")
+
+def newUserVerify():
+    users = input("Please type in your new username: ")
     if(os.path.isfile(users) == False):
         fp = open(users, "w")
         fp.write('Calorie Counter')
         fp.close()
+        print("New " + users + " created")
+    elif(os.path.isfile(users) == True):
+        print("User already exists")
+        newUserVerify()
     else:
-        print("This user already exists")
+        print("User " + users + " does not exist")
+        newUserVerify()
 
 def userVerify():
-    user = input("Please type in you username\n")
-    if(os.path.isfile(user) == True):
+    users = input("Please type in your username: ")
+    if(os.path.isfile(users) == True):
         print("User Verified")
-        verifier = True
+        calorieAdder(users)
     else:
-        print("User " + user + " does not exist")
+        print("User " + users + " does not exist")
         userVerify()
 
-verifier = False
+def calorieAdder(users):
+    userStatus = input("Would you like to add Caloried for " + nowMonth + " " + nowDate + " (yes/no): ") 
+    if(userStatus.lower() == "yes"):
+        print("Please type in the format of (Item-Calories)")
+    elif(userStatus.lower() == "no"):
+        print("What date would you like to add Calories to")
+        month = input("Month: ")
+        date = input("Date: ")
+
 print("Welcome to your personal Calorie Counter\n")
 userStatus = input("Are you a new user (yes/no)")
 
 while(userStatus.lower() != ("yes") or ("no")):
     if(userStatus.lower() == "yes"):
         print("Welcome new user")
-        user = input("Please type in your new username\n")
-        newUserVerify(user)
+        newUserVerify()
         break
     elif(userStatus.lower() == "no"):
         print("Welcome back")
         userVerify()
-        if(verifier == True):
-            print("Would you like to add Calories for " + time.asctime())
         break
     else:
         userStatus = input("Are you a new user (yes/no)")
